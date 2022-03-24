36ffd55bd10c71719711db8041ea603ede71381c
Ashish H <ashishkalburgi90@gmail.com>
2022-03-24 21:29:54 +0530

    fix npm start error

--- src/common/LoadingScreen.js
+++ src/common/LoadingScreen.js
@@ -1,47 +1,35 @@
-// import { css } from "@emotion/core";
-import { Grid, makeStyles } from "@material-ui/core";
+import { css } from "@emotion/core";
+import {
+    Grid,
+    makeStyles
+} from '@material-ui/core'
 import GridLoader from "react-spinners/GridLoader";
 
-// const override = css`
-//   width: 100%;
-//   display: block;
-//   margin: 0 auto;
-//   border-color: #f72a1f;
-// `;
+const override = css`
+  width: 100%;
+  display: block;
+  margin: 0 auto;
+  border-color: #F72A1F;
+`;
 
 const useStyle = makeStyles((theme) => ({
-  loaderBox: {
-    maxWidth: "90px",
-    marginLeft: "auto",
-    marginRight: "auto",
-    height: "100vh",
-  },
-}));
+    loaderBox: {
+        maxWidth: "90px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        height: "100vh"
+
+    }
+}))
 
 function LoadingScreen() {
-  const classes = useStyle();
+    const classes = useStyle();
 
-  return (
-    <Grid
-      container
-      direction="column"
-      justify="center"
-      alignContent="center"
-      className={classes.loaderBox}
-    >
-      <GridLoader
-        color="#F72A1F"
-        loading={true}
-        css={{
-          width: "100%",
-          display: "block",
-          margin: "0 auto",
-          borderColor: "#f72a1f",
-        }}
-        size={20}
-      />
-    </Grid>
-  );
+    return (
+        <Grid container direction="column" justify="center" alignContent="center" className={classes.loaderBox}>
+            <GridLoader color="#F72A1F" loading={true} css={override} size={20} />
+        </Grid>
+    );
 }
 
 export default LoadingScreen;
